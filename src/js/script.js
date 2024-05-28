7d81b37605ef34a6feb1fa8cf5a63f95710ffbb2
Paulo <paulopocas20@gmail.com>
2024-05-28 09:25:07 -0300

    adicionando menu

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('btnMob').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+});
